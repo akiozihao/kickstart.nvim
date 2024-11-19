44c63291d8996aa432b9c066a0dab21718615964
akiozihao <akiozihaoceat@gmail.com>
2024-11-19 18:42:34 +0800

    add float termnial

--- lua/custom/plugins/lspsaga.lua
+++ lua/custom/plugins/lspsaga.lua
@@ -3,6 +3,7 @@ return {
     'nvimdev/lspsaga.nvim',
     config = function()
       require('lspsaga').setup {}
+      vim.keymap.set({ 'n', 't' }, '<A-d>', '<cmd>Lspsaga term_toggle<CR>', { silent = true, noremap = true })
       vim.keymap.set('n', '<leader>so', '<cmd>Lspsaga outline<CR>', { silent = true, noremap = true, desc = '[S]aga [O]utine' })
     end,
     dependencies = {
