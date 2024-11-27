673da64915d04689cd6a0ee1012b860071fb1a99
akiozihao <akiozihaoceat@gmail.com>
2024-11-28 00:43:30 +0800

    add f7 terminal debug

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -106,6 +106,7 @@ return {
     vim.keymap.set('n', '<F4>', dap.step_out)
     vim.keymap.set('n', '<F5>', dap.step_back)
     vim.keymap.set('n', '<F12>', dap.restart)
+    vim.keymap.set('n', '<F7>', dap.close)
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
